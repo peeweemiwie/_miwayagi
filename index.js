7db6f3d0b99f8cef2fd64217df9b930b2c078270
peeweemiwie <miwayagi@gmail.com>
2015-03-01 11:15:52 -0500

    Create a test page

---
+++ index.js
@@ -0,0 +1,29 @@
+var resume = {
+  "work": [
+    {
+      "title": "Design Technologist",
+      "time": "April 2012 to present",
+      "company": "New York Magazine",
+      "skill": "HTML5, CSS3, javascript, jQuery, node.js, wordpress",
+      "detail": [
+        "Responsive Website, Mobile, Tablet, Ad Placement",
+        "nymag, vulture, thecut, bedford+bowery, science of us",
+        "Working closely with sr. developers, designers, product owners, ad sales to develop prototype from PSD."
+      ]
+    },
+      {
+      "title": "Freelance: HTML developer, UI Design, Web Master",
+      "time": "September 2005 to April 2012",
+      "company": "Clients: Standard & Poors, AKA NYC, Manhattan Marketing Ensemble, Tag Worldwide, Exposure New York, Mundocom, Vigil Security, Share cancer support - Japanese, Red Pearl, 2nd skin NYC, Polaris Hair Studio, Sushi Koo",
+      "skill": "wordpress, E-commerce (Prestashop), UI Design, HTML, CSS",
+      "detail": "HTML / CSS development, Flash ad banner, Business to Business Newsletter templates, Email campaign and landing pages"
+    },
+    {
+      "title": "Web Content Administration",
+      "time": "February 2009 to February 2011",
+      "company": "Evolution Store",
+      "skill": "HTML, CSS",
+      "detail": "HTML web page development, Email campaign and landing page"
+    }
+  ]
+}
