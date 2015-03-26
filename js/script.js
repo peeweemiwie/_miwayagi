d702e0564eba5e57371c91bd4197bb7b9b764c34
peeweemiwie <miwayagi@gmail.com>
2015-03-26 08:19:17 -0400

    More clean up for script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 var d = document;
-var firstNavButton = d.querySelectorAll('.page-wrapper header nav a')[0];
-var secondNavButton = d.querySelectorAll('.page-wrapper header nav a')[1];
-var firstFooterNavButton = d.querySelectorAll('.page-wrapper footer nav a')[0];
-var secondFooterNavButton = d.querySelectorAll('.page-wrapper footer nav a')[1];
-var scrollTopButtons = [firstNavButton, firstFooterNavButton];
-var scrollBottomButtons = [secondNavButton, secondFooterNavButton];
-var i=0;
-var j=0;
+var scrollTopButtons = [
+  d.querySelectorAll('.page-wrapper header nav a')[0],
+  d.querySelectorAll('.page-wrapper footer nav a')[0]
+];
+var scrollBottomButtons = [
+  d.querySelectorAll('.page-wrapper header nav a')[1],
+  d.querySelectorAll('.page-wrapper footer nav a')[1]
+];
+var i=0, j=0;
+
 for (i; i<scrollTopButtons.length; i++){
   scrollTopButtons[i].addEventListener('click', function(e){
-    console.log("top");
     e.preventDefault();
     window.scrollTo(0, 0);
   }, true)
 }
 for (j; j<scrollBottomButtons.length; j++){
   scrollBottomButtons[j].addEventListener('click', function(e){
-    console.log('bottom');
     e.preventDefault();
     window.scrollTo(0, d.body.scrollHeight);
   }, true)
